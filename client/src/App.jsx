ed2c8d23cf9ae02ee0dfae953a75afd3f0671030
Jatin Munjal <munjal.jatin24@gmail.com>
2024-01-30 12:28:50 +0530

    initial project and client-side setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-blue-700 text-center">
+        Hello
+      </h1>
+    </div>
+  );
+}
